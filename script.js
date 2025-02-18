461659accce20a4fed6e20992050d2ddb00f32c9
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-02-18 11:04:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const app = document.getElementById("app")
+const users = [
+    {
+        email: 'test@test.com',
+        phone: '99999999999',
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: 'tust@tust.com',
+        phone: '9999999999',
+        ref: 200,
+        refBy: 100
+    }
+
+]
+
 
 const formAction = () => {
     const form = document.getElementById("form")
     form.onsubmit = (event) => {
         event.preventDefault()
+        const formData = new FormData(form)
+        const userData = {
+            email: formData.get('email'),
+            phone: formData.get('phone')
+        }
     }
 }
 
@@ -12,7 +33,7 @@ const startApp = () => {
     `
     <form id="form">
         <input type="email" name="email" id="iemail" placeholder="E-mail">
-        <input type="tel" name="tel" id="itel" placeholder="Telefone">
+        <input type="tel" name="phone" id="iphone" placeholder="Telefone">
         <button>
             Confirmar
         </button>
@@ -20,7 +41,7 @@ const startApp = () => {
     `
 
     app.innerHTML = content
-    
+
     formAction()
 }
 
