c14e0143d91d592498a5891d9d9dcb591dd6ed5e
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-02-23 11:33:50 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -37,18 +37,35 @@ const getTotalSubscribers = (userData) => {
 
 const showInvite = (userData) => {
     app.innerHTML = `
-    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+    <main>
+                <h3>Inscrição confirmada</h3>
 
-    <div id="stats">
-        <h4>
-            ${getTotalSubscribers(userData)}
-        </h4>
-        <p>
-            Inscrições feitas
-        </p>
-    </div>
+                <p>
+                    Convide mais pessoas e concorra a prêmios! <br>
+                    Compartilhe o link e acompanhe as inscrições:
+                </p>
+
+                <div class="input-group">
+                    <label for="link">
+                        <img src="link.svg" alt="Link-icon">
+                    </label>
+                    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+
+                </div>
+            </main>
+
+            <section class="stats">
+                <h4>
+                    ${getTotalSubscribers(userData)}
+                </h4>
+                <p>
+                    Inscrições feitas
+                </p>
+            </section>
     `
+    app.setAttribute('class', 'page-invite')
 
+    updateImageLinks()
 }
 
 const saveUser = (userData) => {
@@ -80,26 +97,74 @@ const formAction = () => {
             const newUser = saveUser(userData)
             showInvite(newUser)
         }
+
     }
 }
 
+const updateImageLinks = () => {
+    document.querySelectorAll('img').forEach((img) => {
+      if (img.src.includes('githubusercontent')) {
+        return
+      }
+      const src = img.getAttribute("src"); 
+      if (src && !src.startsWith("http")) {  
+        img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${src}`;
+      }
+    });
+  };
+
 const startApp = () => {
-    const content = 
-    `
-    <form id="form">
-        <input type="email" name="email" id="iemail" placeholder="E-mail">
-        <input type="tel" name="phone" id="iphone" placeholder="Telefone">
-        <button>
-            Confirmar
-        </button>
-    </form>
+    const content = `
+    <main>
+                <section class="about">
+                    <div class="section-header">
+                        <h2>Sobre o evento</h2>
+                        <span class="badge">AO VIVO</span>
+                    </div>
+                    <p>
+                    Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+                    <br> <br>
+                    Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito 
+                    </p>
+                </section>
+                <section class="registration">
+                    <h2>Inscrição</h2>
+
+                    <form id="form">
+                        <div class="input-wrapper">
+                            <div class="input-group">
+                                <label for="email">
+                                    <img src="mail.svg" alt="Email-icon">
+                                </label>
+                                <input type="email" name="email" id="email" placeholder="E-mail">
+                            </div>
+                            <div class="input-group">
+                                <label for="phone">
+                                    <img src="phone.svg" alt="phone-icon">
+                                </label> 
+                                <input type="number" name="phone" id="phone" placeholder="Telefone">
+                            </div>
+                        </div>
+
+                        <button>
+                            Confirmar
+                            <img src="arrow.svg" alt="Arrow right">
+                        </button>
+                    </form>
+                </section>
+            </main>
     `
 
     app.innerHTML = content
-
+    app.setAttribute('class', 'page-start')
+    updateImageLinks()
     formAction()
 }
-
 startApp()
+// showInvite({
+//    email: 'test@test.com',
+//    phone: '999',
+//    ref: 100
+// })
 
-document.getElementById("logo").onclick = () => startApp()
+document.querySelector("header").onclick = () => startApp()
